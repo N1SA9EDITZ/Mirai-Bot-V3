fae8e3634f7208458fd7538a02cd6696345157dd
✨NI S AN✨ <nisanerror404@gmail.com>
2026-04-02 23:25:53 +0600

    Update handleSchedule.js

--- includes/handle/handleSchedule.js
+++ includes/handle/handleSchedule.js
@@ -2,32 +2,32 @@ const cron = require('node-cron');
 const fs = require('fs');
 const moment = require('moment-timezone');
 const path = require('path');
-const logger = require('../../utils/log');
 
 module.exports = function ({ api, Threads }) {
+
   cron.schedule('*/10 * * * *', async () => {
-    const cc = path.join(__dirname, '..', '..', 'utils', 'data', 'check_data.json');
-    const currentTime = moment().tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss');
-    let lastRunTime = null;
-    if (fs.existsSync(cc)) {
-        const { datetime } = JSON.parse(fs.readFileSync(cc, 'utf-8'));
-        lastRunTime = datetime;
+
+    const file = path.join(__dirname, '../../utils/data/check_data.json');
+    const now = moment().tz('Asia/Dhaka').format('YYYY-MM-DD HH:mm:ss');
+
+    let last = null;
+
+    if (fs.existsSync(file)) {
+      last = JSON.parse(fs.readFileSync(file)).datetime;
     }
-    if (!lastRunTime || moment(currentTime).diff(moment(lastRunTime), 'minutes') >= 10) {
-        const groupList = (await api.getThreadList(100, null, ['INBOX'])).filter(group => group.isSubscribed && group.isGroup);
-        let dataChanged = false;
-        for (const { threadID } of groupList) {
-            const newThreadInfo = await api.getThreadInfo(threadID);
-            const oldThreadInfo = await Threads.getData(threadID);
-            if (JSON.stringify(newThreadInfo) !== JSON.stringify(oldThreadInfo.threadInfo)) {
-                await Threads.setData(threadID, { threadInfo: newThreadInfo });
-                dataChanged = true;
-            }
-        }
-        if (dataChanged) {
-            fs.writeFileSync(cc, JSON.stringify({ datetime: currentTime }));
-            logger(`Tự động cập nhật data của ${groupList.length} box`, '[ DATA ] >');
-        }
+
+    if (!last || moment(now).diff(moment(last), 'minutes') >= 10) {
+
+      const list = await api.getThreadList(50, null, ['INBOX']);
+
+      for (const t of list.filter(i => i.isGroup)) {
+        const info = await api.getThreadInfo(t.threadID);
+        await Threads.setData(t.threadID, { threadInfo: info });
+      }
+
+      fs.writeFileSync(file, JSON.stringify({ datetime: now }, null, 2));
+      console.log("✅ Auto data refresh done");
     }
   });
-}
+
+};
