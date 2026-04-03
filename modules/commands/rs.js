dd5555d5452a6c08e8c4d3d6af8b58ddedb88c48
✨NI S AN✨ <nisanerror404@gmail.com>
2026-04-03 13:23:12 +0600

    Update rs.js

--- modules/commands/rs.js
+++ modules/commands/rs.js
@@ -1,11 +1,30 @@
-this.config = {
+module.exports.config = {
     name: "rs",
-    version: "1.0.0",
-    hasPermssion: 3,
-    credits: "DongDev",
-    description: "Khởi Động Lại Bot.",
+    version: "1.0.1",
+    hasPermssion: 2,
+    credits: "N1SA9",
+    description: "Restart the bot",
     commandCategory: "Admin",
     cooldowns: 0,
-    images: [],
- };
- this.run = ({event, api}) => api.sendMessage("✅", event.threadID, () => process.exit(1), event.messageID)
+    images: []
+};
+
+module.exports.run = async function ({ event, api }) {
+    try {
+        return api.sendMessage(
+            "🔄 Restarting bot...",
+            event.threadID,
+            () => {
+                process.exit(1);
+            },
+            event.messageID
+        );
+    } catch (e) {
+        console.error(e);
+        return api.sendMessage(
+            "❎ Failed to restart bot",
+            event.threadID,
+            event.messageID
+        );
+    }
+};
