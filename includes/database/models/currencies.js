ad7ac50e03ff2bd1c6826338b8bcb280cccbcbbb
✨NI S AN✨ <nisanerror404@gmail.com>
2026-04-03 09:00:37 +0600

    Update currencies.js

--- includes/database/models/currencies.js
+++ includes/database/models/currencies.js
@@ -1,26 +1,35 @@
-module.exports = function({ sequelize, Sequelize }) {
-	let Currencies = sequelize.define('Currencies', {
-		num: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		userID: {
-			type: Sequelize.BIGINT,
-			unique: true
-		},
-		money: {
-			type: Sequelize.BIGINT,
-			defaultValue: 0
-		},
-        exp: {
-            type: Sequelize.BIGINT,
-            defaultValue: 0
-        },
-		data: {
-			type: Sequelize.JSON
-		}
-	});
+module.exports = function ({ sequelize, Sequelize }) {
 
-	return Currencies;
-}
+  const Currencies = sequelize.define("Currencies", {
+
+    num: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    userID: {
+      type: Sequelize.STRING, // FIXED (BIGINT → STRING)
+      unique: true,
+      allowNull: false
+    },
+
+    money: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    },
+
+    exp: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    },
+
+    data: {
+      type: Sequelize.JSON,
+      defaultValue: {}
+    }
+
+  });
+
+  return Currencies;
+};
