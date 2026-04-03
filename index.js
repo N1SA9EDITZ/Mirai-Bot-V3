1341af7cdea3129ad860170451c1f4101f0a474c
✨NI S AN✨ <nisanerror404@gmail.com>
2026-04-03 14:27:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const { spawn } = require("child_process");
 const logger = require("./utils/log");
 
+global.countRestart = 0;
+
 function startBot(message) {
-    if (message) logger(message, "[ KuRuMi-V3 ]");
+  if (message) logger(message, "[ KuRuMi-V3 ]");
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "kurumi.js"], {
-        cwd: __dirname,
-        stdio: "inherit",
-        shell: true
-    });
+  const child = spawn("node", ["kurumi.js"], {
+    cwd: __dirname,
+    stdio: "inherit",
+    shell: true
+  });
 
-    child.on("close", (codeExit) => {
-        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
-            startBot("🔄 Restarting KuRuMi-V3...");
-            global.countRestart += 1;
-            return;
-        }
-    });
+  child.on("close", (codeExit) => {
+    if ((codeExit !== 0) && global.countRestart < 5) {
+      global.countRestart++;
+      startBot("🔄 Restarting KuRuMi-V3...");
+    }
+  });
 
-    child.on("error", function (error) {
-        logger("Error: " + JSON.stringify(error), "[ KuRuMi-V3 ]");
-    });
-};
+  child.on("error", (error) => {
+    logger("Error: " + JSON.stringify(error), "[ KuRuMi-V3 ]");
+  });
+}
 
 startBot();
