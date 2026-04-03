d52f90e6f380301aa58fefb813f9e5ac5a545932
✨NI S AN✨ <nisanerror404@gmail.com>
2026-04-03 12:25:50 +0600

    Update users.js

--- includes/controllers/users.js
+++ includes/controllers/users.js
@@ -8,80 +8,56 @@ module.exports = function ({ models, api }) {
 	async function getNameUser(id) {
 		try {
 			if (global.data.userName.has(id)) return global.data.userName.get(id);
-			else if (global.data.allUserID.includes(id)) {
-				const nameUser = (await this.getData(id)).name;
-				if (nameUser) return nameUser;
-				else return "Người dùng facebook";
-			} else return "Người dùng facebook";
+
+			const data = await getData(id);
+			if (data && data.name) return data.name;
+
+			return "Facebook User";
+		} catch {
+			return "Facebook User";
 		}
-		catch { return "Người dùng facebook" }
 	}
 
 	async function getAll(...data) {
-		var where, attributes;
+		let where, attributes;
 		for (const i of data) {
-			if (typeof i != 'object') throw global.getText("users", "needObjectOrArray");
 			if (Array.isArray(i)) attributes = i;
-			else where = i;
+			else if (typeof i === "object") where = i;
 		}
-		try {
-			return (await Users.findAll({ where, attributes })).map(e => e.get({ plain: true }));
-		}
-		catch (error) {
-			console.error(error);
-			throw new Error(error);
-		}
+		return (await Users.findAll({ where, attributes }))
+			.map(e => e.get({ plain: true }));
 	}
 
 	async function getData(userID) {
-		try {
-			const data = await Users.findOne({ where: { userID } });
-			if (data) return data.get({ plain: true });
-			else return false;
-		}
-		catch(error) {
-			console.error(error);
-			throw new Error(error);
-		}
+		const data = await Users.findOne({ where: { userID } });
+		return data ? data.get({ plain: true }) : false;
 	}
 
 	async function setData(userID, options = {}) {
-		if (typeof options != 'object' && !Array.isArray(options)) throw global.getText("users", "needObject");
-		try {
-			(await Users.findOne({ where: { userID } })).update(options);
+		let data = await Users.findOne({ where: { userID } });
+
+		if (!data) {
+			await createData(userID, options);
 			return true;
 		}
-		catch (error) {
-			try {
-				await this.createData(userID, options);
-			} catch (error) {
-				console.error(error);
-				throw new Error(error);
-			}
-		}
+
+		await data.update(options);
+		return true;
 	}
 
 	async function delData(userID) {
-		try {
-			(await Users.findOne({ where: { userID } })).destroy();
-			return true;
-		}
-		catch (error) {
-			console.error(error);
-			throw new Error(error);
-		}
+		const data = await Users.findOne({ where: { userID } });
+		if (!data) return false;
+		await data.destroy();
+		return true;
 	}
 
 	async function createData(userID, defaults = {}) {
-		if (typeof defaults != 'object' && !Array.isArray(defaults)) throw global.getText("users", "needObject");
-		try {
-			await Users.findOrCreate({ where: { userID }, defaults });
-			return true;
-		}
-		catch (error) {
-			console.error(error);
-			throw new Error(error);
-		}
+		await Users.findOrCreate({
+			where: { userID },
+			defaults
+		});
+		return true;
 	}
 
 	return {
